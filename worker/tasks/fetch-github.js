eaf92d4e30383fd94073c1d8ad0d8c9ae818f719
john tekdek <johntekdek@gmail.com>
2019-12-30 14:27:43 +0000

    work up til fetch data

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,27 @@
+var fetch = require('node-fetch');
+
+
+const baseURL = 'http://jobs.github.com/positions.json'
+
+async function fetchGithub(){
+
+    let resultCount = 1  , onPage = 0 ;
+    const allJobs = [];
+
+    while(resultCount > 0) {
+        const res = await  fetch(`${baseURL}?page=${onPage}`);
+        const jobs = await res.json();
+        allJobs.push(jobs)
+        resultCount = jobs.length;
+        console.log('got',jobs.length);
+        onPage ++;
+
+    }
+    console.log('got',allJobs.length)
+}
+
+fetchGithub()
+
+module.exports = fetchGithub
+
+
