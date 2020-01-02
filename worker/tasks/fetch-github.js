87aa33e71b08fc6485fc55e57d04960b2563cace
john tekdek <johntekdek@gmail.com>
2020-01-02 01:04:27 +0000

    sunday work

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,23 +1,23 @@
 var fetch = require('node-fetch');
 
 
-const baseURL = 'http://jobs.github.com/positions.json'
+const baseURL = 'https://jobs.github.com/positions.json'
 
-async function fetchGithub(){
+async function fetchGithub() {
 
-    let resultCount = 1  , onPage = 0 ;
+    let resultCount = 1, onPage = 0;
     const allJobs = [];
 
-    while(resultCount > 0) {
-        const res = await  fetch(`${baseURL}?page=${onPage}`);
+    while (resultCount > 0) {
+        const res = await fetch(`${baseURL}?page=${onPage}`);
         const jobs = await res.json();
         allJobs.push(jobs)
         resultCount = jobs.length;
-        console.log('got',jobs.length);
-        onPage ++;
+        console.log('got', jobs.length);
+        onPage++;
 
     }
-    console.log('got',allJobs.length)
+    console.log('got', allJobs.length)
 }
 
 fetchGithub()
